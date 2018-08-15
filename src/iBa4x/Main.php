d21a60c0520c990d79be18e7a6f4ded4e21be27a
Khaled_Craft1b <iiBa4x@gmail.com>
2018-08-15 10:18:54 +0300

    Update Main.php

--- src/iBa4x/Main.php
+++ src/iBa4x/Main.php
@@ -1,2 +1,19 @@
 <?php
-namespaes: iBa4x
+namespace: iBa4x;
+# this plugin by iBa4x
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\math\Vector3;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info(TextFormat::GREEN . "By iBa4x");
+    @mkdir($this->getDataFolder());
+    $config = new Config($this->getDataFolder()."config.yml",Config::YAML);
+  }
+  
+}
