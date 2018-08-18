7d0cd919c399944bba1e7683d80922cd33f0ecf0
Khaled_Craft1b <iiBa4x@gmail.com>
2018-08-18 11:48:06 +0300

    Update Main.php

--- src/iBa4x/Main.php
+++ src/iBa4x/Main.php
@@ -4,9 +4,13 @@ namespace: iBa4x;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\math\Vector3;
 use pocketmine\utils\TextFormat;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 
 class Main extends PluginBase implements Listener{
   public function onEnable(){
@@ -15,5 +19,33 @@ class Main extends PluginBase implements Listener{
     @mkdir($this->getDataFolder());
     $config = new Config($this->getDataFolder()."config.yml",Config::YAML);
   }
-  
+  public function onCommand(CommandSender $sender,Command $command, $label,array $args){
+		switch($command->getName()){
+			case "Join":
+				$name = $sender->getPlayer()->getName();
+				$config = new Config($this->getDataFolder()."config.yml",Config::YAML);
+        
+			return true;
+		}
+	}
+  public function onDeath(PlayerDeathEvent $event){
+		$entity = $event->getEntity();
+		$cause = $entity->getLastDamageCause();
+		if($entity instanceof Player){
+			$name = $entity->getName();
+			$deaths = new Config($this->getDataFolder()."config.yml",Config::YAML);
+      $deaths->set($name,$deaths->get($name) - 2);
+			$deaths->save();
+		}
+		if($cause instanceof EntityDamageByEntityEvent){
+			$killer = $event->getEntity()->getLastDamageCause()->getDamage();
+			if($killer instanceof Player){
+				$kills = new Config($this->getDataFolder()."config.yml",Config::YAML);
+				$name = $killer->getName();
+				$kills->set($name,$kills->get($name) + 1);
+				$kills->save();
+				}
+			}
+		}
+	}
 }
